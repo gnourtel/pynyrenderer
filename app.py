9a9d6c15c1df1e1ece249aae4bbaa8e82fd3f78a
Truong Le <truong.le@jamalex.io>
2022-04-06 18:25:30 +0700

    add wip

---
+++ app.py
@@ -0,0 +1,30 @@
+from math import abs
+from PIL import Image
+from PIL.ImageColor import getcolor
+
+white = getcolor('white', 'RGB')
+black = getcolor('black', 'RGB')
+red   = getcolor('red', 'RGB')
+
+def line(x0: int, y0: int, x1: int, y1: int, image: Image.Image, color: tuple) -> None:
+    steep = False
+    if abs(x1 - x0) < abs(y1 - y0):
+        steep = True
+        x1,
+    for x in range(x0, x1+1):
+        t = (x - x0) / (x1 - x0)
+        y = int(y0 + t * (y1 - y0))
+        image.putpixel((x, y), color)
+
+def main():
+    image = Image.new('RGB', (100, 100), black)
+    
+    line(13, 20, 80, 40, image, white)
+    line(20, 13, 40, 80, image, red)
+    line(80, 40, 13, 20, image, red)
+
+    image = image.transpose(1) # Flip vertical
+    image.show()
+
+if __name__ == "__main__":
+    main()
